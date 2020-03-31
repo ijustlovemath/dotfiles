ab6b0ad40b9b2fca2b262aae43bcff75e2b162a3
Jeremy DeJournett <dejourn2@illinois.edu>
2020-03-30 21:34:20 -0400

    automated youcompleteme (untested)

---
+++ vim.sh
@@ -0,0 +1,28 @@
+# make the bundle directory
+mkdir -p ~/.vim/bundle
+# TODO: make variable for ~/.vim/bundle/Vundle.vim
+
+# get vundle
+ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# install vundle in vimrc
+cat << EOF >> ~/.vimrc
+set nocompatible
+filetype off
+
+set rtp+=~/.vim/bundle/Vundle.vim
+call vundle#begin()
+Plugin 'VundleVim/Vundle.vim'
+Plugin 'ycm-core/YouCompleteMe'
+call vundle#end()
+filetype plugin indent on
+EOF
+
+# change .vim to unix using
+find ~/.vim/bundle -type f -name "*.vim" -exec dos2unix {} \;
+
+# execute PluginInstall
+vim -c 'PluginInstall'
+
+#install c languages (or just all, lol)
+python3 ~/.vim/bundle/YouCompleteMe/install.py --all
